47af6d0f7e4d100b532d9aa8f8db5dc5a1644994
BlakeDavis28 <blake_davis@outlook.com>
2020-08-07 09:23:43 +1000

    homepage css

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ class Home extends Component{
   render(){
     return(
 
-      <div>
+      <div id="stuff">
       <h1> Good Little Helpers</h1>
       <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa molestiae autem doloribus accusantium magnam quis nam! Ipsa molestiae id sequi! Commodi totam voluptas, minus quos natus laboriosam libero. Perspiciatis, quos!</p>
       <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente nulla aliquam iusto aut veniam, nesciunt vel aperiam voluptatem. Sed totam, nisi autem amet voluptatibus esse, soluta nemo cum provident ab.</p>
